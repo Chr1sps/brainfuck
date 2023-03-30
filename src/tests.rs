8c4cbc01d18d27dcd97e08f8d933090cd5756ae3
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2023-03-30 02:10:51 +0200

    Readded the wrapping options removed in commit 89a94d57c4a494eebee27946ee8103f03964c35e.
    This reverts commit 89a94d57c4a494eebee27946ee8103f03964c35e.

--- src/tests.rs
+++ src/tests.rs
@@ -7,30 +7,48 @@ use crate::Token;
 use super::{BrainfuckMachine, Lexer, Parser};
 #[test]
 fn test_machine_index_change_base() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, false);
     assert_eq!(machine.index, 0);
     machine.move_right(5);
     assert_eq!(machine.index, 5);
     machine.move_left(3);
     assert_eq!(machine.index, 2);
 }
-
 #[test]
-#[should_panic = "Index out of bounds."]
-fn test_machine_index_change_left_panic() {
-    let mut machine = BrainfuckMachine::new(10);
+fn test_machine_index_change_no_wrap() {
+    let mut machine = BrainfuckMachine::new(10, false, false);
     machine.move_left(3);
+    assert_eq!(
+        machine.index, 0,
+        "Left wrap doesn't pass, index: {}.",
+        machine.index
+    );
+    machine.move_right(10);
+    assert_eq!(
+        machine.index, 9,
+        "Right wrap doesn't pass, index: {}.",
+        machine.index
+    );
 }
 #[test]
-#[should_panic = "Index out of bounds."]
-fn test_machine_index_change_right_panic() {
-    let mut machine = BrainfuckMachine::new(10);
-    machine.move_right(11);
+fn test_machine_index_change_wrap() {
+    let mut machine = BrainfuckMachine::new(10, true, false);
+    machine.move_left(3);
+    assert_eq!(
+        machine.index, 7,
+        "Left wrap doesn't pass, index: {}.",
+        machine.index
+    );
+    machine.move_right(6);
+    assert_eq!(
+        machine.index, 3,
+        "Right wrap doesn't pass, index: {}.",
+        machine.index
+    );
 }
-
 #[test]
 fn test_machine_value_change_base() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, true);
     assert_eq!(
         machine.tape[machine.index], 0,
         "Current cell value: {}.",
@@ -43,7 +61,7 @@ fn test_machine_value_change_base() {
 }
 #[test]
 fn test_machine_value_change_wrap() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, true);
     assert_eq!(
         machine.tape[machine.index], 0,
         "Current cell value: {}.",
@@ -62,10 +80,31 @@ fn test_machine_value_change_wrap() {
         machine.tape[machine.index]
     );
 }
-
+#[test]
+fn test_machine_value_change_no_wrap() {
+    let mut machine = BrainfuckMachine::new(10, false, false);
+    assert_eq!(
+        machine.tape[machine.index], 0,
+        "Current cell value: {}.",
+        machine.tape[machine.index]
+    );
+    machine.substract(11);
+    assert_eq!(
+        machine.tape[machine.index], 0,
+        "Current cell value: {}.",
+        machine.tape[machine.index]
+    );
+    machine.add(255);
+    machine.add(255);
+    assert_eq!(
+        machine.tape[machine.index], 255,
+        "Current cell value: {}.",
+        machine.tape[machine.index]
+    );
+}
 #[test]
 fn test_machine_put_char() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, false);
     machine.add(65);
     let result: char = machine.put_char();
     assert_eq!(
@@ -76,14 +115,14 @@ fn test_machine_put_char() {
 }
 #[test]
 fn test_machine_read_char() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, false);
     machine.read_char('A');
     let result = machine.tape[machine.index];
     assert_eq!(result, 65, "Different char read. Char read: {}.", result);
 }
 #[test]
 fn test_machine_check_loop() {
-    let mut machine = BrainfuckMachine::new(10);
+    let mut machine = BrainfuckMachine::new(10, false, false);
     assert!(!machine.check_loop());
     machine.add(4);
     assert!(machine.check_loop());
